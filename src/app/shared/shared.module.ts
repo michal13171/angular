a213ff5b371a955f16989231c33272b00d06b2e5
michal13171 <michal13171@wp.pl>
2018-04-06 18:11:45 +0200

    validate

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Input, Directive } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { AbstractControl, ValidatorFn, NG_VALIDATORS, Validator } from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent
-  ],
+  imports: [CommonModule],
+  exports: [HeaderComponent],
   declarations: [HeaderComponent]
 })
-export class SharedModule { }
+
+export class SharedModule {}
